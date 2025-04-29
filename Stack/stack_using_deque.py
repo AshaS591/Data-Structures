f74664dd8e8000b4481c8cfdcf2e8226667b9b5e
AshaS591 <ashas17205@gmail.com>
2025-04-29 16:54:54 +0530

    Update stack_using_deque.py
    stack implementation using deque

--- Stack/stack_using_deque.py
+++ Stack/stack_using_deque.py
@@ -1,1 +1,32 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.stack=deque()
+       
+    def push(self,data):
+        self.stack.append(data)
+        print(f'{data} pushed on to the stack successfully...')
+    def pop(self):
+        res=self.stack.pop()
+        print(f'{res} is removed from stack successfully...')
+    def is_empty(self):
+        if len(self.stack)==0:
+            print('Stack is empty')
+        else:
+            print('Stack is not empty...')
+    def peek(self):
+        print(f'Top element of stack {self.stack} is {self.stack[-1]}')
+    def size(self):
+        print(f'Size of stack {self.stack} is {len(self.stack)}')
+            
+stack =Stack()
+stack.push(10)
+stack.push(20)
+stack.push(40)
+stack.size()
+stack.is_empty()
+stack.push(60)
+stack.push(90)
+stack.pop()
 
